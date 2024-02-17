366fcb317e4c3b20a49ebcc7245ec186b264a444
Benoît Soupart <71172532+benoit160@users.noreply.github.com>
2024-02-17 17:09:47 +0100

    Welcome screen (#5)

--- SpaceBattles.Client/Program.cs
+++ SpaceBattles.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace SpaceBattles.Client
 {
@@ -11,7 +12,8 @@ namespace SpaceBattles.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
