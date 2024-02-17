a70b5ebab907161d54b20f6acdbbe36cfd8a7d15
Benoît Soupart <71172532+benoit160@users.noreply.github.com>
2024-02-17 22:41:01 +0100

    IRequirements (#9)

--- SpaceBattles.Core/Domain/Entities/Building/BuildingLevel.cs
+++ SpaceBattles.Core/Domain/Entities/Building/BuildingLevel.cs
@@ -1,11 +1,31 @@
+using SpaceBattles.Core.Domain.Enums;
+using SpaceBattles.Core.Domain.Interfaces;
+using SpaceBattles.Core.Domain.Records;
+
 namespace SpaceBattles.Core.Domain.Entities.Building;
 
 #nullable disable annotations
-public sealed class BuildingLevel
+public sealed class BuildingLevel : IRequirements
 {
     public Building Building { get; init; }
     
     public short BuildingId { get; init; }
 
     public short Level { get; set; }
+    
+    public long TitaniumCost => (long)(Building.TitaniumCost * Math.Pow(Building.ScalingFactor, Level));
+    
+    public long SiliconCost  => (long)(Building.SiliconCost * Math.Pow(Building.ScalingFactor, Level));
+    
+    public long HeliumCost  => (long)(Building.HeliumCost * Math.Pow(Building.ScalingFactor, Level));
+    
+    IEnumerable<ResourceCost> IRequirements.Costs
+    {
+        get
+        {
+            yield return new ResourceCost(Resource.Titanium, TitaniumCost);
+            yield return new ResourceCost(Resource.Silicon, SiliconCost);
+            yield return new ResourceCost(Resource.Helium, HeliumCost);
+        }
+    }
 }
---
+++ SpaceBattles.Tests/Domain/Interfaces/IRequirementsTests.cs
@@ -0,0 +1,52 @@
+﻿using SpaceBattles.Core.Domain.Entities.Universe;
+using SpaceBattles.Core.Domain.Enums;
+using SpaceBattles.Core.Domain.Interfaces;
+
+namespace SpaceBattles.Tests.Domain.Interfaces;
+
+public class RequirementsTests
+{
+    private const int StartingTitanium = 150;
+    private const int StartingSilicon = 75;
+    private const int StartingHelium = 0;
+    
+    [Theory]
+    [InlineData(1, true)]
+    [InlineData(2, false)]
+    [InlineData(3, true)]
+    [InlineData(4, false)]
+    [InlineData(5, false)]
+    [InlineData(6, false)]
+    [InlineData(7, false)]
+    [InlineData(8, false)]
+    [InlineData(9, false)]
+    [InlineData(10, false)]
+    public void HasEnoughResources_Building(short buildingId, bool expectedResult)
+    {
+        // Arrange
+        Planet planet = new Planet();
+        IRequirements building = planet.Buildings.First(b => b.BuildingId == buildingId);
+
+        // Act
+        bool sufficient = planet.HasEnoughResource(building);
+        
+        // Asset 
+        Assert.Equal(expectedResult, sufficient);
+    }
+    
+    [Fact]
+    public void ConsumeResources_Building()
+    {
+        // Arrange
+        Planet planet = new Planet();
+        IRequirements building = planet.Buildings.First();
+
+        // Act
+        planet.ConsumeResources(building);
+        
+        // Asset 
+        Assert.Equal(StartingTitanium - building.TitaniumCost, planet[Resource.Titanium]);
+        Assert.Equal(StartingSilicon - building.SiliconCost, planet[Resource.Silicon]);
+        Assert.Equal(StartingHelium - building.HeliumCost, planet[Resource.Helium]);
+    }
+}
