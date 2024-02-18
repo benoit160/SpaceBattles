0ed0ec6507b229706f074b62962a60cfee6c9397
Benoît Soupart <71172532+benoit160@users.noreply.github.com>
2024-02-18 18:06:17 +0100

    Game state service (#13)

---
+++ SpaceBattles.Core/Application/Services/GameState.cs
@@ -0,0 +1,13 @@
+﻿using SpaceBattles.Core.Domain.Entities.Universe;
+
+namespace SpaceBattles.Core.Application.Services;
+
+public sealed class GameState
+{
+    public Planet CurrentPlanet { get; private set; }
+
+    public GameState()
+    {
+        CurrentPlanet = new Planet();
+    }
+}
