5d30593f7689fa380c8f7f130a39ea7455be9d88
Benoît <benoit160@live.be>
2024-02-28 18:06:12 +0100

    wip

--- SpaceBattles.Tests/Domain/Buildings/BuildingLevelTests.cs
+++ SpaceBattles.Tests/Domain/Buildings/BuildingLevelTests.cs
@@ -32,7 +32,7 @@ public class BuildingLevelTests
     
     [Theory]
     [InlineData(1, -11)]
-    [InlineData(2, -25)]
+    [InlineData(2, -24)]
     [InlineData(3, -40)]
     [InlineData(4, -59)]
     [InlineData(5, -81)]
@@ -44,6 +44,41 @@ public class BuildingLevelTests
 
         // Act
         int actualEnergy = planet.Buildings[0].Energy;
+        
+        // Assert
+        Assert.Equal(expectedConsumption, actualEnergy);
+    }
+    
+    [Theory]
+    [InlineData(1, 22)]
+    [InlineData(2, 48)]
+    [InlineData(3, 80)]
+    [InlineData(4, 117)]
+    [InlineData(5, 161)]
+    public void SolarPlant_Production(short level, int expectedConsumption)
+    {
+        // Arrange
+        Planet planet = new Planet();
+        planet.Buildings[6].Level = level;
+
+        // Act
+        int actualEnergy = planet.Buildings[6].Energy;
+        
+        // Assert
+        Assert.Equal(expectedConsumption, actualEnergy);
+    }
+    
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(2, 0)]
+    public void OtherBuilding_NoEnergyUsage(short level, int expectedConsumption)
+    {
+        // Arrange
+        Planet planet = new Planet();
+        planet.Buildings[8].Level = level;
+
+        // Act
+        int actualEnergy = planet.Buildings[8].Energy;
         
         // Assert
         Assert.Equal(expectedConsumption, actualEnergy);
