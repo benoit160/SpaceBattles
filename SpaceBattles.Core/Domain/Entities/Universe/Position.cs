9dab3d2d9a240332ced0d7ad05b8e084c19885f2
Benoît <benoit160@live.be>
2024-03-16 13:08:04 +0100

    wip

---
+++ SpaceBattles.Core/Domain/Entities/Universe/Position.cs
@@ -0,0 +1,50 @@
+﻿namespace SpaceBattles.Core.Domain.Entities.Universe;
+
+public abstract class Position : IEquatable<Position>
+{
+    public byte Galaxy { get; init; }
+    
+    public byte SolarSystem { get; init; }
+    
+    public byte Slot { get; init; }
+
+    /// <summary>
+    /// Computes the distance to the other object, to determine duration of spaceflight between
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public int DistanceTo(Position other)
+    {
+        // Distance between a planet and it's moon for exemple
+        if(Equals(other))
+            return 5;
+
+        int distanceBetweenGalaxies = Galaxy == other.Galaxy ? 
+            0 : Math.Abs(Galaxy - other.Galaxy) * 20_000;
+
+        int distanceBetweenSolarSystems = SolarSystem == other.SolarSystem ?
+            0 : 2_700 + Math.Abs(SolarSystem - other.SolarSystem) * 95;
+
+        int distanceBetweenSlots = Slot == other.Slot ?
+            0 : 1_000 + Math.Abs(Slot - other.Slot) * 5;
+
+        return distanceBetweenGalaxies + distanceBetweenSolarSystems + distanceBetweenSlots;
+    }
+
+    /// <summary>
+    /// Computes the distance between the 2 objects, to determine duration of spaceflight between
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public static int DistanceBetween(Position first, Position second) => first.DistanceTo(second);
+
+    /// <summary>
+    /// Indicates whether the coordinates points to the same position.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public bool Equals(Position? other)
+    {
+        return other is not null && (Slot == other.Slot && Galaxy == other.Galaxy && SolarSystem == other.SolarSystem);
+    }
+}
