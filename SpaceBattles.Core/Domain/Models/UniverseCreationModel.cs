efcfad3fd14961d211ccaf33d95b6d75c4d6cfa7
Benoît <benoit160@live.be>
2024-04-14 20:51:15 +0200

    w

--- SpaceBattles.Core/Domain/Models/UniverseCreationModel.cs
+++ SpaceBattles.Core/Domain/Models/UniverseCreationModel.cs
@@ -7,17 +7,11 @@ public class UniverseCreationModel
 {
     public UniverseCreationModel()
     {
-        UniverseName = string.Empty;
         CommanderName = "Player";
         StartingPlanetName = "Earth";
         UniverseSpeed = 1f;
         UniverseSize = UniverseSize.Medium;
     }
-
-    [Required(ErrorMessage = "Please enter a name")]
-    [StringLength(20, ErrorMessage = "Universe name can't be more than 20 characters")]
-    [MinLength(3, ErrorMessage = "Universe name must be at least 3 characters")]
-    public string UniverseName { get; set; }
 
     [Required(ErrorMessage = "Please enter a name")]
     [StringLength(20, ErrorMessage = "Your commander name can't be more than 20 characters")]
