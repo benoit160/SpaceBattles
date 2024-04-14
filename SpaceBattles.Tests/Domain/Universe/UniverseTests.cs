06a780bb72f2c16f7a04ad00e56be6de4cc77e95
Benoît <benoit160@live.be>
2024-04-14 11:28:53 +0200

    w

---
+++ SpaceBattles.Tests/Domain/Universe/UniverseTests.cs
@@ -0,0 +1,27 @@
+﻿using SpaceBattles.Core.Domain.Enums;
+using SpaceBattles.Core.Domain.Models;
+
+namespace SpaceBattles.Tests.Domain.Universe;
+
+public class UniverseTests
+{
+    [Fact]
+    public void NoDuplicatePlanets()
+    {
+        // Arrange
+        UniverseCreationModel model = new UniverseCreationModel
+        {
+            UniverseSize = UniverseSize.VeryLarge,
+        };
+
+        // Act
+        Core.Domain.Entities.Universe.Universe universe = Core.Domain.Entities.Universe.Universe.CreateUniverse(model);
+        
+        // Assert
+        foreach(var grouping in universe.Planets.GroupBy(planet => (planet.Galaxy, planet.SolarSystem)))
+        {
+            HashSet<byte> images = grouping.Select(x => x.ImageIndex).ToHashSet();
+            Assert.Equal(grouping.Select(x => x.ImageIndex).Count(), images.Count);
+        }
+    }
+}
