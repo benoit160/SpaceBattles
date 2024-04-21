7c9e06d6e9ca40ed33ddec0a10d529cd06974a08
Benoît <benoit160@live.be>
2024-04-21 15:19:06 +0200

    wip

--- SpaceBattles.Tests/Application/Services/StatisticServiceTests.cs
+++ SpaceBattles.Tests/Application/Services/StatisticServiceTests.cs
@@ -1,10 +1,18 @@
-﻿using SpaceBattles.Core.Application.Services;
+﻿using Moq;
+using SpaceBattles.Core.Application.Services;
 using SpaceBattles.Core.Domain.Models;
 
 namespace SpaceBattles.Tests.Application.Services;
 
 public class StatisticServiceTests
 {
+    private readonly INotificationService _notificationService;
+
+    public StatisticServiceTests()
+    {
+        _notificationService = new Mock<INotificationService>().Object;
+    }
+    
     [Fact]
     public void GetPlanetStatistics()
     {
@@ -21,6 +29,52 @@ public class StatisticServiceTests
         // Assert
         Assert.Equal(0, statistics.TotalTitaniumProduced);
         Assert.Equal(0, statistics.TotalSiliconProduced);
+        Assert.Equal(0, statistics.TotalHeliumProduced);
+    }
+    
+    [Fact]
+    public void ResourcesProduced()
+    {
+        // Arrange
+        UniverseCreationModel model = new UniverseCreationModel();
+        GameState state = new GameState();
+        state.Initialize(model);
+        
+        StatisticService service = new StatisticService(state);
+        PlanetStatistics statistics = service[state.CurrentPlanet];
+        DateTime timeBeforeUpdated = state.CurrentPlanet.LastUpdated;
+
+        PlanetService planetService = new PlanetService(state, service, _notificationService);
+
+        // Act
+        planetService.UpdateCurrentPlanet(timeBeforeUpdated + TimeSpan.FromMinutes(10));
+
+        // Assert
+        Assert.Equal(300, statistics.TotalTitaniumProduced);
+        Assert.Equal(150, statistics.TotalSiliconProduced);
+        Assert.Equal(0, statistics.TotalHeliumProduced);
+    }
+    
+    [Fact]
+    public void ResourcesProduced_StillCounting_AfterMaximumCapacityReached()
+    {
+        // Arrange
+        UniverseCreationModel model = new UniverseCreationModel();
+        GameState state = new GameState();
+        state.Initialize(model);
+        
+        StatisticService service = new StatisticService(state);
+        PlanetStatistics statistics = service[state.CurrentPlanet];
+        DateTime timeBeforeUpdated = state.CurrentPlanet.LastUpdated;
+
+        PlanetService planetService = new PlanetService(state, service, _notificationService);
+
+        // Act
+        planetService.UpdateCurrentPlanet(timeBeforeUpdated + TimeSpan.FromDays(10));
+
+        // Assert
+        Assert.True(statistics.TotalTitaniumProduced > 10_000);
+        Assert.True(statistics.TotalSiliconProduced > 10_000);
         Assert.Equal(0, statistics.TotalHeliumProduced);
     }
 }
