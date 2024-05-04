b0609f7346f1b0e65cd283eb4f9f7a5867730a9e
Benoît Soupart <71172532+benoit160@users.noreply.github.com>
2024-05-04 14:25:55 +0200

    Refactor IServiceCollectionExtensions (#69)

--- SpaceBattles.Client/Services/ServicesExtensions.cs
+++ SpaceBattles.Client/Services/ServicesExtensions.cs
@@ -6,11 +6,6 @@ public static class ServicesExtensions
 {
     public static void AddSpaceBattlesServices(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddScoped<GameState>();
-        serviceCollection.AddScoped<PlanetService>();
-        serviceCollection.AddScoped<StatisticService>();
-        serviceCollection.AddScoped<SaveService>();
-        serviceCollection.AddScoped<BotService>();
         serviceCollection.AddScoped<INotificationService, NotificationService>();
         serviceCollection.AddScoped<BrowserService>();
     }
---
+++ SpaceBattles.Core/Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattles.Core.Application.Extensions;
+
+using Microsoft.Extensions.DependencyInjection;
+using SpaceBattles.Core.Application.Services;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCoreSpaceBattlesServices(this IServiceCollection services)
+    {
+        services.AddScoped<GameState>();
+        services.AddScoped<PlanetService>();
+        services.AddScoped<StatisticService>();
+        services.AddScoped<SaveService>();
+        services.AddScoped<BotService>();
+    }
+}
