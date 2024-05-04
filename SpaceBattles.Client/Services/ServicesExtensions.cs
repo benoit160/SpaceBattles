9b8540a646af3a67458666859ed518f0645cbf3d
Benoît <benoit160@live.be>
2024-05-04 14:13:18 +0200

    w

--- SpaceBattles.Client/Services/ServicesExtensions.cs
+++ SpaceBattles.Client/Services/ServicesExtensions.cs
@@ -12,6 +12,6 @@ public static class ServicesExtensions
         serviceCollection.AddScoped<SaveService>();
         serviceCollection.AddScoped<BotService>();
         serviceCollection.AddScoped<INotificationService, NotificationService>();
-        serviceCollection.AddScoped<BrowserService>();
+        serviceCollection.AddScoped<IBrowserService,  BrowserService>();
     }
 }
