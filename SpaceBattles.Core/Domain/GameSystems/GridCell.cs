5d2dab57f852e739e480992ecb1071126a8db82a
Benoît Soupart <71172532+benoit160@users.noreply.github.com>
2024-05-05 21:54:08 +0200

    ItemGrid (#77)

---
+++ SpaceBattles.Core/Domain/GameSystems/GridCell.cs
@@ -0,0 +1,46 @@
+﻿namespace SpaceBattles.Core.Domain.GameSystems;
+
+using System.Diagnostics.CodeAnalysis;
+
+public class GridCell<T>
+    where T : class, IGridItem
+{
+    public T? Item { get; private set; }
+
+    public bool IsEmpty => Item is null;
+
+    /// <summary>
+    /// Inserts a  new item in the slot.
+    /// </summary>
+    public bool Insert(T item)
+    {
+        if (Item is null)
+        {
+            Item = item;
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Delete the item from the slot.
+    /// </summary>
+    public void Clear()
+    {
+        Item = null;
+    }
+
+    /// <summary>
+    /// Tries to remove the item from the slot, and gives it back.
+    /// </summary>
+    public bool TryRemove([NotNullWhen(true)]out T? item)
+    {
+        item = default;
+        if (Item is null || !Item.CanBeRemoved) return false;
+
+        item = Item;
+        Item = default;
+        return true;
+    }
+}
