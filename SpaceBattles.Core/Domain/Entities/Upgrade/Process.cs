80f277d92f87fc651194635a8836bd5ef30faf38
Benoît <benoit160@live.be>
2024-05-20 11:08:10 +0200

    w

---
+++ SpaceBattles.Core/Domain/Entities/Upgrade/Process.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceBattles.Core.Domain.Entities.Upgrade;
+
+public abstract class Process
+{
+    public DateTime Start { get; init; }
+
+    public int WorkNeeded { get; init; }
+
+    public int WorkDone { get; set; }
+
+    public int RemainingWork => WorkNeeded - WorkDone;
+
+    public TimeSpan EstimatedDuration(double production)
+        => TimeSpan.FromSeconds(RemainingWork / production);
+
+    public DateTime EstimatedEnd(double production)
+        => Start + EstimatedDuration(production);
+
+    public bool IsFinished => RemainingWork <= 0;
+
+    public double PercentFinished => 100 * (WorkDone / WorkNeeded);
+}
