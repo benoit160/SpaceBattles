53fc04a24b882269533d7694ab368fab61cc77ae
Benoît Soupart <71172532+benoit160@users.noreply.github.com>
2024-07-14 13:21:19 +0200

    Mock time provider (#58)

---
+++ SpaceBattles.Core/Application/Services/TimeProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattles.Core.Application.Services;
+
+public sealed class TimeProvider : ITimeProvider
+{
+    private readonly PeriodicTimer _timer;
+
+    public TimeProvider(int secondsInterval)
+    {
+        _timer = new(TimeSpan.FromSeconds(secondsInterval));
+    }
+
+    public DateTime Now => DateTime.Now;
+
+    public ValueTask<bool> WaitForNextTickAsync(CancellationToken cancellationToken)
+        => _timer.WaitForNextTickAsync(cancellationToken);
+}
