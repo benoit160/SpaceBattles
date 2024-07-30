8bb5b60cfe3e2494dae1494d4c564d8b5015663e
Benoît <benoit160@live.be>
2024-07-30 21:06:35 +0200

    w

---
+++ SpaceBattles.Core/Domain/Technology/TechnologyLevel.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceBattles.Core.Domain.Technology;
+
+using System.Text.Json.Serialization;
+using SpaceBattles.Core.Domain.Interfaces;
+
+public sealed class TechnologyLevel : IRequirements
+{
+    public int TechnologyId { get; init; }
+
+    [JsonIgnore]
+    public required Technology Technology { get; set; }
+
+    public short Level { get; set; }
+
+    public long TitaniumCost => (long)(Technology.TitaniumCost * Math.Pow(2, Level));
+
+    public long SiliconCost => (long)(Technology.SiliconCost * Math.Pow(2, Level));
+
+    public long HeliumCost => (long)(Technology.HeliumCost * Math.Pow(2, Level));
+}
