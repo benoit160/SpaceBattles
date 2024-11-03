87da069cab6a4e222164c96310cd15f8da6694cc
Benoît <benoit160@live.be>
2024-11-03 20:00:24 +0100

    w

--- SpaceBattles.Core/Application/Services/SaveService.cs
+++ SpaceBattles.Core/Application/Services/SaveService.cs
@@ -1,5 +1,9 @@
-﻿namespace SpaceBattles.Core.Application.Services;
+﻿using System.Net.Http.Json;
 
+namespace SpaceBattles.Core.Application.Services;
+
+using System.IO.Compression;
+using System.Text;
 using System.Text.Json;
 using SpaceBattles.Core.Domain.Entities.Battle;
 using SpaceBattles.Core.Domain.Entities.Building;
@@ -9,14 +13,16 @@ public sealed class SaveService
 {
     private const string Key = "SaveData";
 
+    private readonly HttpClient _httpClient;
     private readonly GameState _gameState;
     private readonly StatisticService _statistics;
     private readonly IBrowserService _browserService;
 
     private readonly JsonSerializerOptions _options;
 
-    public SaveService(GameState gameState, StatisticService statisticService, IBrowserService browserService)
+    public SaveService(HttpClient httpClient, GameState gameState, StatisticService statisticService, IBrowserService browserService)
     {
+        _httpClient = httpClient;
         _gameState = gameState;
         _statistics = statisticService;
         _browserService = browserService;
@@ -27,11 +33,29 @@ public sealed class SaveService
         };
     }
 
+    public async Task SaveToCloud()
+    {
+        PostDataRequest request = new PostDataRequest(null, GetCompressedSaveFile());
+        await _httpClient.PostAsJsonAsync("api/save", request);
+    }
+
+    public record PostDataRequest(Guid? SaveId, string SaveData);
+
     public void SaveToStorage()
     {
         string game = JsonSerializer.Serialize(_gameState.CurrentUniverse, _options);
 
         _browserService.WriteToLocalStorage(Key, game);
+    }
+
+    public string GetCompressedSaveFile()
+    {
+        string saveData = JsonSerializer.Serialize(_gameState.CurrentUniverse, _options);
+
+        byte[] gzipped = Zip(saveData);
+        string b64 = Base64Encode(gzipped);
+
+        return b64;
     }
 
     public string? GetLoadData()
@@ -91,6 +115,42 @@ public sealed class SaveService
 
             planet.Spaceships = planet.BattleUnits.AsMemory(8, 10);
             planet.Defenses = planet.BattleUnits.AsMemory(0, 8);
+        }
+    }
+
+    public string Base64Encode(ReadOnlySpan<byte> bytes)
+        => Convert.ToBase64String(bytes);
+
+    public byte[] Base64Decode(string value)
+        => Convert.FromBase64String(value);
+
+    public static byte[] Zip(string str)
+    {
+        var bytes = Encoding.UTF8.GetBytes(str);
+
+        using (var msi = new MemoryStream(bytes))
+        using (var mso = new MemoryStream())
+        {
+            using (var gs = new GZipStream(mso, CompressionMode.Compress))
+            {
+                msi.CopyTo(gs);
+            }
+
+            return mso.ToArray();
+        }
+    }
+
+    public static string Unzip(byte[] bytes)
+    {
+        using (var msi = new MemoryStream(bytes))
+        using (var mso = new MemoryStream())
+        {
+            using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+            {
+                gs.CopyTo(mso);
+            }
+
+            return Encoding.UTF8.GetString(mso.ToArray());
         }
     }
 }
