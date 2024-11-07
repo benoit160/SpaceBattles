46270b5112493b83320a2aa38187cf39be8f0e2f
Benoît <benoit160@live.be>
2024-11-07 21:25:02 +0100

    w

--- SpaceBattles.E2E.Tests/IndexTests.cs
+++ SpaceBattles.E2E.Tests/IndexTests.cs
@@ -24,4 +24,45 @@ public class Tests : BlazorTest
 
         Assert.IsNotEmpty(requests.Where(r => r.Url.Contains("/api/telemetry") && r.Method == "POST"));
     }
+    
+    [Test]
+    public async Task Play()
+    {
+        List<IRequest> requests = [];
+        
+        Page.Request += (_, request) => requests.Add(request);
+        
+        await Page.GotoAsync(RootUri.AbsoluteUri);
+        
+        await Page.GetByRole(AriaRole.Button, new() { NameString = "Play" }).ClickAsync();
+        
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync("SpaceBattles");
+
+        Assert.IsNotEmpty(requests.Where(r => r.Url.Contains("/api/telemetry") && r.Method == "POST"));
+    }
+    
+    [TestCase("overview")]
+    [TestCase("buildings")]
+    [TestCase("shipyard")]
+    [TestCase("defenses")]
+    [TestCase("universe")]
+    [TestCase("statistics")]
+    public async Task PageNavigation(string buttonName)
+    {
+        List<IRequest> requests = [];
+        
+        Page.Request += (_, request) => requests.Add(request);
+        
+        await Page.GotoAsync(RootUri.AbsoluteUri);
+        
+        await Page.GetByRole(AriaRole.Button, new() { NameString = "play" }).ClickAsync();
+        
+        await Page.GetByRole(AriaRole.Link, new() { NameString = buttonName }).ClickAsync();
+        
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync("SpaceBattles");
+
+        Assert.IsNotEmpty(requests.Where(r => r.Url.Contains("/api/telemetry") && r.Method == "POST"));
+    }
 }
