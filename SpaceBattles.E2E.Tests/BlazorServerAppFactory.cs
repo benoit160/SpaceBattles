6d930807aa14e68578459c1d109c8361958b9332
Benoît <benoit160@live.be>
2024-11-07 20:25:18 +0100

    w

--- SpaceBattles.E2E.Tests/BlazorServerAppFactory.cs
+++ SpaceBattles.E2E.Tests/BlazorServerAppFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 
 namespace SpaceBattles.E2E.Tests;
 
@@ -8,11 +9,15 @@ public class BlazorServerAppFactory<TProgram>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureServices(_ =>
+        builder.ConfigureAppConfiguration((_, config) =>
         {
-            Environment.SetEnvironmentVariable("CosmosDB:EndpointUrl", "localhost:8081");
-            Environment.SetEnvironmentVariable("CosmosDB:AccountKey", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
-            Environment.SetEnvironmentVariable("CosmosDB:DatabaseName", "SpaceBattles.E2E");
+            Dictionary<string, string> settings = new Dictionary<string, string>
+            {
+                ["CosmosDB:EndpointUrl"]  = "https://localhost:8081",
+                ["CosmosDB:AccountKey"]  = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                ["CosmosDB:DatabaseName"]  = "SpaceBattles.E2E",
+            };
+            config.AddInMemoryCollection(settings);
         });
 
         builder.UseEnvironment("Development");
