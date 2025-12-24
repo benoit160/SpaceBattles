d954b5142d63610a2f02c9316d8d2d7f9407c9c3
Benoît <benoit160@live.be>
2025-12-24 17:11:00 +0100

    wip

---
+++ SpaceBattles.Playwright/FactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace SpaceBattles.Playwright;
+
+public class FactoryTests : BlazorTest
+{
+    [Test]
+    public async Task CreateFactory()
+    {
+        // Arrange
+        WebApplicationFactory<Server.Program> factory = new();
+        HttpClient client = factory.CreateClient();
+
+        // Act
+        HttpResponseMessage result = await client.GetAsync("/");
+
+        // Assert
+        await Assert.That(result.StatusCode).IsEqualTo(HttpStatusCode.OK);
+    }
+}
---
+++ SpaceBattles.Playwright/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Blazor.Playwright;
+
+global using Microsoft.AspNetCore.Mvc.Testing;
+
+global using BlazorTest = Blazor.Playwright.BlazorTest<Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactory<SpaceBattles.Server.Program>, SpaceBattles.Server.Program>;
